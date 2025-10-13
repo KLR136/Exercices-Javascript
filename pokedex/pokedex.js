0d79ab4a0fb8b2cfb50708b37fab2fe7958a1c0d
Nathan CAMPANINI <145757176+KLR136@users.noreply.github.com>
2025-10-13 13:50:55 +0200

    corrected a typo in the filename and added new Pokemon to the Pokedex

--- pokedex/pokedex.js
+++ pokedex/pokedex.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 const http = require('http');
 
-const description = fs.readFileSync('./fichier.txt', 'utf-8', (error, text) => {
+const server = http.createServer((req, res) => {
+    console.log(request.url);
+    res.end('Hello World');});
+server.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
+
+const description = fs.readFileSync('../fichier.txt', 'utf-8', (error, text) => {
     if (error) {
         return console.error('Error reading file:', error);
     }
